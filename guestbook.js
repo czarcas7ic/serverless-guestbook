a6d008228ebf7a4c71b2e6d675944398a5643065
Adam Tucker <40078083+czarcas7ic@users.noreply.github.com>
2021-06-26 22:31:04 -0400

    added API URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d4182dca.us-south.apigw.appdomain.cloud/guestbook/entries';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
